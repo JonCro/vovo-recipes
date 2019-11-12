7bffd61e9262bfd2618cf9f775f914e6c4b2fbb2
Jon Cronin <jcronin42@gmail.com>
2019-11-11 20:45:32 -0500

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About VoVo and her family recipes</h1>
+      <p>Vovo was my grandmother. She was a typical grandmother who showed her love in many ways, one of those being with food. This site is to share her recipes with others to spread her love.</p>
+    </div>
+  )
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Vovo's Family Recipes</h1>
+      <h2>Meals to feed your family for generations</h2>
+    </div>
+  )
+}
+
+export default IndexPage
