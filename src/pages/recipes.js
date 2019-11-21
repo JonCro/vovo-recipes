74ca93afbcb9bbc51a965ead2db78dfa18a6b6f7
Jon Cronin <jcronin42@gmail.com>
2019-11-21 14:51:34 -0500

    added condition for recipes not having an image

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -36,10 +36,11 @@ const RecipesPage = () => {
       <Head title="Recipes" />
       <h1>Recipes</h1>
       <ol className={recipesStyles.recipes}>
-          {data.allContentfulRecipes.edges.map(post => (
+          {data.allContentfulRecipes.edges.map(post => 
+          (
             <li className={recipesStyles.card} key={post.node.slug}>
               <Link to={`/recipes/${post.node.slug}`}>
-                <img src={post.node.image.file.url} alt="" />
+                {post.node.image !== null ? <img src={post.node.image.file.url} alt="" /> : <p>no image</p>}
                 <h2>{post.node.title}</h2>
                 <div className={recipesStyles.cardMeta}>
                   <p>{post.node.date}</p>
