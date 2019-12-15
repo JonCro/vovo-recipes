bb4fb593989e18571597067e287703415c01f46e
Jon Cronin <jcronin42@gmail.com>
2019-12-15 12:29:56 -0500

    format cleanup, changed recipe tag style

--- src/templates/recipes.js
+++ src/templates/recipes.js
@@ -3,7 +3,7 @@ import { graphql, Link } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout';
-import blogStyles from '../pages/recipe.module.scss';
+import recipeStyles from '../pages/recipe.module.scss';
 import Head from '../components/head';
 
 export const query = graphql`
@@ -12,42 +12,44 @@ export const query = graphql`
         slug: {
           eq: $slug
         }
-    ) {
-        title,
-        tags,
-        categories {
-          type,
-          slug
-        },
-        ingredients {
-          json
-        },
-        instructions {
-          json
-        },
-        image {
-          file {
-            url
-          }
+    ) 
+    {
+      title,
+      tags,
+      categories {
+        type,
+        slug
+      },
+      ingredients {
+        json
+      },
+      instructions {
+        json
+      },
+      image {
+        file {
+          url
         }
+      }
     }
   }
 `
 
 const Recipes = (props) => {
+  console.log(props)
   const data = props.data.contentfulRecipes;
-  console.log('recipes template')
-  console.log(data)
+  // console.log('recipes template')
+  // console.log(data)
   return (
     <Layout>
       <Head title={`${data.title}`} recipe={true} />
       <h1>{data.title}</h1>
       {data.image !== null ? <img src={data.image.file.url} alt="" /> : <p>no image</p>}
-      <div className={blogStyles.meta}>
-        <span className={blogStyles.tags}>tags</span>
-        {data.categories.map((data) => {
+      <div className={recipeStyles.meta}>
+        <span className={recipeStyles.tags}>tags</span>
+        {data.categories.map((data, i) => {
           return (
-            <Link to={`/recipes/tags/${data.slug}`} ><span className={blogStyles.tag}>{data.type}</span></Link>
+            <Link to={`/recipes/tags/${data.slug}`} ><span className={recipeStyles.tag}>{data.type}</span></Link>
           )
         })}
       </div>
